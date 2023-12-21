745bd104e31966f6128c5e9c0b4df872c8302227
Adam Pan <adampan94@gmail.com>
2023-12-21 00:26:06 -0800

    Init frontend & backend setup

---
+++ backend/equilibriafit/healthfit/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    # Define user profile fields
+    pass
+
+class Workout(models.Model):
+    # Define workout fields
+    pass
+
+class NutritionPlan(models.Model):
+    # Define nutrition plan fields
+    pass
